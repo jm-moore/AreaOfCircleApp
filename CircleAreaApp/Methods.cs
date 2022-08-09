a8cd95e22eb8dd62746a3c2b9e51aac3d65e7abe
jm-moore <jmmoore9358@gmail.com>
2022-08-09 15:13:32 -0500

    initial commit

---
+++ CircleAreaApp/Methods.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleAreaApp
+{
+    public class Methods
+    {
+        public static void AreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var answer = Math.PI * radius * radius;
+            Console.WriteLine($"{Math.Round(answer, 2)} is the area of your circle.");
+        }
+    }
+}
